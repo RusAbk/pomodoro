526f97ea88127aab00cabd9b34fbc0612b11c475
RusAbk <RusAbk@users.noreply.github.com>
2025-10-10 08:05:12 +0000

    deploy: 8483db0bb8535d31cd9e739937fa9664531d01b0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"flutter.js": "888483df48293866f9f41d3d9274a779",
 "canvaskit/canvaskit.js.symbols": "58832fbed59e00d2190aa295c4d70360",
 "canvaskit/skwasm.js.symbols": "0088242d10d7e7d6d2649d1fe1bda7c1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "fb8f83ec1718319c958bb0a2118f63a9",
+"flutter_bootstrap.js": "224073fadf8daa4d4af4bde3d714f989",
 "version.json": "01ce5b03cfdc7f7b69eb2f69e7ddadb1",
 "main.dart.js": "1d0f383de48ee9c43f867be415244bf9"};
 // The application shell files that are downloaded before a service worker can
